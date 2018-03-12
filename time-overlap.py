bbd7aaee9b4c78c17447f770c15eb892a46b42a1
Madhu-Guddana <36655676+Madhu-Guddana@users.noreply.github.com>
2018-03-12 13:44:58 +0530

    Update time-overlap.py

--- time-overlap.py
+++ time-overlap.py
@@ -1,3 +1,9 @@
+"""This package Provides alogirthm to check overlap timings
+Along with unittests
+
+"""
+import time
+
 test_set = [{
     'is_overlapping': False,
     'time_ranges': [{
@@ -26,7 +32,7 @@ test_set = [{
         'end': '2016-01-25 16:10:23+00:00'
     }, ]
 }, {
-    'is_overlapping': False,
+    'is_overlapping': True,
     'time_ranges': [{
         'start': '2016-01-21 14:21:00+00:00',
         'end': '2016-01-21 14:22:10+00:00'
@@ -76,18 +82,47 @@ test_set = [{
 }]
 
 
+def isOverlapping(array):
+  """
+  Method to check if time ranges in array is overlapping or not
+  Args:
+    array(list): List containng array ranges
 
-def isOverlapping(arr):
-    #Find out whether all the time ranges in arr overlap mutially or not.
-    pass
+  Returns:
+    bool: True if time overlaps False otherwise
+  """
+  pattern = '%Y-%m-%d %H:%M:%S+00:00'
+  for entry in array:
+    start = int(time.mktime(time.strptime(entry['start'], pattern)))
+    end = int(time.mktime(time.strptime(entry['end'], pattern)))
+    entry['start'] = start
+    entry['end'] = end
+  array.sort(key=lambda ele: ele['start'])
 
+  overlapping =  False
+  for index in range(len(array)-1):
+    first = array[index]
+    second = array[index+1]
+    if first['end'] > second['start']:
+      overlapping = True
+  return overlapping
+
+
+time_ranges= [{
+        'start': '2016-01-25 10:00:25+00:00',
+        'end': '2016-01-26 10:00:12+00:00'
+    }, {
+        'start': '2016-01-21 11:25:00+00:00',
+        'end': '2016-01-25 16:10:23+00:00'
+    }, ]
 
 def run(test_cases):
     for test_case in test_cases:
-        if test_case['is_overlapping'] == isOverlapping(test_cases['time_ranges']):
-            print 'Passing!'
-        else:
-            print 'Failing'
+      if test_case['is_overlapping'] == isOverlapping(test_case['time_ranges']):
+          print 'Passing!'
+      else:
+          print test_case
+          print 'Failing'
 
 
 run(test_set)
